1725ffec149b7d1d630451641f71bd705046aa84
Brian Mitchell <bman4789@gmail.com>
2015-03-28 19:18:09 -0500

    Can now add color, updated card model

--- client/app/add/add.controller.js
+++ client/app/add/add.controller.js
@@ -3,10 +3,35 @@
 angular.module('hciPrototypeApp')
   .controller('AddCtrl', function ($scope, $http) {
     $scope.card = {};
+    $scope.card.color = [];
+
+    $scope.toggleSelection = function toggleSelection(color) {
+      if ($scope.card.color.indexOf(color) === -1) {
+        $scope.card.color.push(color);
+      } else {
+        $scope.card.color.splice($scope.card.color.indexOf(color), 1);
+      }
+    };
+
+    $scope.set_color = function (color) {
+      if (color == "red") {
+        return {color: "#F9A084"}
+      } else if (color == "green") {
+        return {color: "#90CDA4"}
+      } else if (color == "black") {
+        return {color: "#C6BBB9"}
+      } else if (color == "blue") {
+        return {color: "#A1DCF9"}
+      } else if (color == "white") {
+        return {color: "#FFFCD1"}
+      } else {
+        return {color: "#FF00F9"}
+      }
+    };
 
     $scope.addCard = function() {
-      if($scope.cardName === '' || $scope.cardDescription === '' || $scope.cardType === '') {
-        return;
+      if($scope.card.color.length == 0) {
+        $scope.card.color.push("white");
       }
       $http.post('/api/cards', $scope.card).success(function(){
         $scope.reset();
@@ -15,5 +40,6 @@ angular.module('hciPrototypeApp')
 
     $scope.reset = function() {
       $scope.card = {};
+      $scope.card.color = [];
     };
   });
--- server/api/card/card.model.js
+++ server/api/card/card.model.js
@@ -7,12 +7,16 @@ var CardSchema = new Schema({
   name: String,
   description: String,
   type: String,
+  subType: String,
   deck: String,
   color: [],
-  attributes: {
-    power: Number,
-    toughness: Number
-  }
+  colorID: [],
+  rulesText: [],
+  flavor: String,
+  power: Number,
+  toughness: Number,
+  rarity: String,
+  blockSet: String
 });
 
 module.exports = mongoose.model('Card', CardSchema);
